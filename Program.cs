b2c599fe9b811eddaabaf3800f9c4e40961b46d0
VictorHugo55 <davichugopereira@gmail.com>
2025-05-15 15:12:55 -0300

    atualizando o program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 
+using challenger.Domain.Entities;
+using challenger.Infrastructure.Context;
+using challenger.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace challenger
 {
     public class Program
@@ -12,7 +18,30 @@ namespace challenger
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = builder.Configuration["Swagger:Title"],
+                    Description = builder.Configuration["Swagger:Description"] + DateTime.Now.Year,
+                    Contact = new OpenApiContact()
+                    {
+                        Email = "rm558550@fiap.com.br",
+                        Name = "Victor Hugo"
+                    }
+                });
+            });
+
+            builder.Services.AddDbContext<CGContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+            });
+
+            builder.Services.AddScoped<IRepository<Moto>, Repository<Moto>>(); // genérico, opcional se usar específico
+            builder.Services.AddScoped<IMotoRepository, MotoRepository>();
+
+            builder.Services.AddScoped<IRepository<Patio>, Repository<Patio>>();
+            builder.Services.AddScoped<IPatioRepository, PatioRepository>();
 
             var app = builder.Build();
 
